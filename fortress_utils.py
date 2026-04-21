62f2a8b5660bcf06db44ce9f3ca99b82a293f4c1
Danya Zh <danilazhum@gmail.com>
2026-04-21 10:33:44 -0400

    Specified group name, added notes on common pitfalls

--- fortress_utils.py
+++ fortress_utils.py
@@ -4,15 +4,20 @@
 # Archives on Fortress are expected at:
 #   {GROUP_PATH}/{year}_Reconstructed/{archive_id}{TAR_SUFFIX}
 # e.g. /group/nolte/2024_Reconstructed/20241102SM_A.tar
-GROUP_PATH = '/group/your_group'
+GROUP_PATH = '/group/nolte'
 TAR_SUFFIX = '_A.tar'
+#             ^ Sometimes this standard is not upheld. Check manually
+
 
 # File to pull out of each tar during extraction.
+# You will have to tinker if you want to extract pattern
 TARGET_FILE = 'parameters.m'
 
 
-def tar_path_for(archive):
-  '''Returns the Fortress path for the tar corresponding to `archive`.
-  Assumes the first 4 characters of the id are the year.'''
-  year = archive[:4]
+def tar_path_for(archive_id):
+  '''
+  Returns the Fortress path for the tar corresponding to `archive_id`.
+  Assumes the first 4 characters of the id are the year.
+  '''
+  year = archive_id[:4]
   return f"{GROUP_PATH}/{year}_Reconstructed/{archive}{TAR_SUFFIX}"
